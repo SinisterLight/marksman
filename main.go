c6178cdbee2133b6c7ee727107e8b624b5b69dba
Hari haran <hariharan.uno@gmail.com>
2015-08-06 15:10:18 +0530

    add basic agents handler

--- main.go
+++ main.go
@@ -1,14 +1,22 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
 	"log"
 	"net/http"
+
+	"gopkg.in/mgo.v2"
 )
 
 const (
 	metricsPath = "/metrics"
+	agentsPath  = "/agents"
+)
+
+var (
+	ca *mgo.Collection
 )
 
 func main() {
@@ -22,7 +30,18 @@ func main() {
 	flag.Parse()
 
 	http.HandleFunc(metricsPath, metricsHandler)
+	http.HandleFunc(agentsPath, agentsHandler)
 	http.Handle("/", http.FileServer(http.Dir("./public")))
+
+	session, err := mgo.Dial("localhost")
+	if err != nil {
+		panic(err)
+	}
+	defer session.Close()
+
+	// Optional. Switch the session to a monotonic behavior.
+	session.SetMode(mgo.Monotonic, true)
+	ca = session.DB("recon-dev").C("agents")
 
 	log.Println("Server started: http://localhost" + *addr)
 	log.Fatal(http.ListenAndServe(*addr, nil))
@@ -30,4 +49,50 @@ func main() {
 
 func metricsHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "TODO")
+}
+
+func agentsHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "GET":
+		var R []Agent
+		err := ca.Find(nil).All(&R)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		enc := json.NewEncoder(w)
+		if err := enc.Encode(R); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		return
+	case "POST":
+		var a Agent
+		dec := json.NewDecoder(r.Body)
+		if err := dec.Decode(&a); err != nil {
+			http.Error(w, "unable to decode json", http.StatusBadRequest)
+			return
+		}
+		if a.UID == "" {
+			http.Error(w, "UID can't be empty", http.StatusBadRequest)
+			return
+		}
+		err := ca.Insert(a)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		w.WriteHeader(http.StatusCreated)
+		w.Write([]byte(a.UID))
+		return
+	default:
+		http.Error(w, "method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+}
+
+// Agent represents a recon agent running on
+// a machine.
+type Agent struct {
+	UID string
 }
