fde0be2088a5ea37874fd1ada99ec8425cfa2ee3
Hari haran <hariharan.uno@gmail.com>
2015-08-06 12:55:30 +0530

    use the name metrics instead of stats

--- main.go
+++ main.go
@@ -2,28 +2,32 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
 	"net/http"
 )
 
 const (
-	statsPath = "/stats"
+	metricsPath = "/metrics"
 )
 
 func main() {
 	log.SetFlags(0)
 	log.SetPrefix("marksman: ")
+
 	var (
 		addr = flag.String("addr", ":8080", "serve HTTP on `address`")
 	)
+
 	flag.Parse()
 
-	http.HandleFunc(statsPath, statsHandler)
+	http.HandleFunc(metricsPath, metricsHandler)
 	http.Handle("/", http.FileServer(http.Dir("./public")))
+
 	log.Println("Server started: http://localhost" + *addr)
 	log.Fatal(http.ListenAndServe(*addr, nil))
 }
 
-func statsHandler(w http.ResponseWriter, r *http.Request) {
-	panic("TODO")
+func metricsHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "TODO")
 }
