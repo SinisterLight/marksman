190994fe1ce34464766133ef031e7c713cf5c402
Hari haran <hariharan.uno@gmail.com>
2015-09-03 13:18:16 +0530

    add PATCH for policies, fix #3

--- policy_handler.go
+++ policy_handler.go
@@ -51,7 +51,36 @@ func policyHandler(w http.ResponseWriter, r *http.Request) {
 		// TODO: if the policy is successfully sent to the agent, store the agent's PolicyConfig in the master server's DB as well.
 		return
 	case "PATCH":
-		// TODO: implement
+		var p policy.Policy
+		defer r.Body.Close()
+		dec := json.NewDecoder(r.Body)
+		if err := dec.Decode(&p); err != nil {
+			http.Error(w, "unable to decode json", http.StatusBadRequest)
+			return
+		}
+		if p.AgentUID == "" {
+			http.Error(w, "UID can't be empty", http.StatusBadRequest)
+			return
+		}
+		var a Agent
+		err := agentsC.Find(bson.M{"uid": p.AgentUID}).One(&a)
+		if err != nil {
+			http.Error(w, "recond agent unknown: check the agent UID and try again", http.StatusInternalServerError)
+			return
+		}
+		if a.Status() == "offline" {
+			http.Error(w, "recond agent offline: restart the agent and try again", http.StatusInternalServerError)
+			return
+		}
+		var reply string
+		if err := natsEncConn.Request(p.AgentUID+"_modify_policy", &p, &reply, 5*time.Second); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		if reply != "modify_policy_ack" {
+			http.Error(w, reply, http.StatusInternalServerError)
+			return
+		}
 	case "DELETE":
 		var p policy.Policy
 		defer r.Body.Close()
